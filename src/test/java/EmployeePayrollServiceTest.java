6c433f84be4b0a2b8ff661224d6711f6c7ad1ce5
WaseemAkhtar032 <waseemakhtar032@gmail.com>
2022-12-29 19:53:22 +0530

    Added: Show number of entries

---
+++ src/test/java/EmployeePayrollServiceTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import uc1employeepayrollservicetest.IOService;
+import uc1readwriteemployeepayroll.EmployeePayrollData;
+import uc1readwriteemployeepayroll.EmployeePayrollService;
+
+public class EmployeePayrollServiceTest {
+
+    /**
+     * Method to find the number of entries given3Employees here When Written To
+     * File Should Match the EmployeeEntries
+     */
+    @Test
+    public void given3Employees_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+
+        /**
+         * created the array of employee Payroll data to store the entries
+         */
+        EmployeePayrollData[] arrayOfEmp = { new EmployeePayrollData(1, "Deven Mali", 100000.0),
+                new EmployeePayrollData(2, "Pavan Patil", 200000.0),
+                new EmployeePayrollData(3, "Kaplesh Mali", 300000.0) };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmp));
+
+        /**
+         * calling the method writeEmployeeData from the IO_File
+         */
+        employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+
+        /**
+         * calling the method countEntries from the IO_File
+         */
+        long entries = employeePayrollService.countEntries(IOService.FILE_IO);
+        assertEquals(3, entries);
+    }
+}
